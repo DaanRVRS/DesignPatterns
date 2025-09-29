d6ad3ed8845b5e633e3e5a13af1aa1411f4104ec
Daan Reuvers <77575704+DaanRVRS@users.noreply.github.com>
2025-09-29 14:16:55 +0200

    Add files via upload

---
+++ BeverageFactory.cs
@@ -0,0 +1,201 @@
+﻿using DecoratorPattern.Beverages;
+using DecoratorPattern.Condiments;
+
+namespace DecoratorPattern
+{
+    internal class BeverageFactory
+    {
+        static void PrintBeverage(Beverage beverage)
+        {
+            Console.WriteLine(beverage.GetDescription() + " " + beverage.size + " $ " + beverage.cost().ToString("#.##"));
+        }
+        public Beverage createBeverage(DrinkType type, Size size)
+        {
+            Beverage beverage;
+
+            switch (type)
+            {
+                case DrinkType.Espresso:
+                    beverage = new Espresso();
+                    break;
+                case DrinkType.Doppio:
+                    beverage = new Espresso();
+                    beverage = new EspressoCondiment(beverage);
+                    break;
+                case DrinkType.Lungo:
+                    beverage = new Espresso();
+                    beverage = new Water(beverage);
+                    break;
+                case DrinkType.Macchiato:
+                    beverage = new Espresso();
+                    beverage = new Milk_Foam(beverage);
+                    break;
+
+                case DrinkType.Corretta:
+                    beverage = new Espresso();
+                    beverage = new Liqour(beverage);
+                    break;
+
+                case DrinkType.ConPanna:
+                    beverage = new Espresso();
+                    beverage = new Whip(beverage);
+                    break;
+
+                case DrinkType.Cappuccino:
+                    beverage = new Espresso();
+                    beverage = new Steamed_Milk(beverage);
+                    beverage = new Milk_Foam(beverage);
+                    break;
+
+                case DrinkType.Americano:
+                    beverage = new Espresso();
+                    beverage = new Water(beverage);
+                    beverage = new Water(beverage);
+                    break;
+
+                case DrinkType.CaffeLatte:
+                    beverage = new Espresso();
+                    beverage = new Steamed_Milk(beverage);
+                    beverage = new Steamed_Milk(beverage);
+                    beverage = new Milk_Foam(beverage);
+                    break;
+
+                case DrinkType.FlatWhite:
+                    beverage = new Espresso();
+                    beverage = new EspressoCondiment(beverage);
+                    beverage = new Steamed_Milk(beverage);
+                    beverage = new Milk_Foam(beverage);
+                    break;
+
+                case DrinkType.Romana:
+                    beverage = new Espresso();
+                    beverage = new Lemon(beverage);
+                    break;
+
+                case DrinkType.Marocchino:
+                    beverage = new Espresso();
+                    beverage = new ChocolateCondiment(beverage);
+                    beverage = new Milk_Foam(beverage);
+                    break;
+
+                case DrinkType.Mocha:
+                    beverage = new Espresso();
+                    beverage = new ChocolateCondiment(beverage);
+                    beverage = new Steamed_Milk(beverage);
+                    beverage = new Whip(beverage);
+                    break;
+
+                case DrinkType.Bicerin:
+                    beverage = new Espresso();
+                    beverage = new ChocolateCondiment(beverage);
+                    beverage = new White_Chocolate(beverage);
+                    beverage = new Whip(beverage);
+                    break;
+
+                case DrinkType.Breve:
+                    beverage = new Espresso();
+                    beverage = new Milk_Foam(beverage);
+                    beverage = new Steamed_Milk(beverage);
+                    break;
+
+                case DrinkType.RafCoffee:
+                    beverage = new Espresso();
+                    beverage = new Cream(beverage);
+                    beverage = new Vanilla_Sugar(beverage);
+                    break;
+
+                case DrinkType.MeadRaf:
+                    beverage = new Espresso();
+                    beverage = new Honey(beverage);
+                    beverage = new Cream(beverage);
+                    break;
+
+                case DrinkType.Galao:
+                    beverage = new Espresso();
+                    beverage = new Milk_Foam(beverage);
+                    beverage = new Milk_Foam(beverage);
+                    break;
+
+                case DrinkType.CaffeAffogato:
+                    beverage = new Espresso();
+                    beverage = new EspressoCondiment(beverage);
+                    beverage = new Ice_Cream(beverage);
+                    break;
+
+                case DrinkType.ViennaCoffee:
+                    beverage = new Espresso();
+                    beverage = new Whip(beverage);
+                    beverage = new Whip(beverage);
+                    beverage = new EspressoCondiment(beverage);
+                    break;
+
+                case DrinkType.Glace:
+                    beverage = new Espresso();
+                    beverage = new Ice_Cream(beverage);
+                    break;
+
+                case DrinkType.ChocolateMilk:
+                    beverage = new Chocolate();
+                    beverage = new Milk(beverage);
+                    beverage = new Milk(beverage);
+                    break;
+
+                case DrinkType.DemiCreme:
+                    beverage = new Espresso();
+                    beverage = new Cream(beverage);
+                    beverage = new Cream(beverage);
+                    beverage = new EspressoCondiment(beverage);
+                    break;
+
+                case DrinkType.LatteMacchiato:
+                    beverage = new Espresso();
+                    beverage = new Steamed_Milk(beverage);
+                    beverage = new Steamed_Milk(beverage);
+                    beverage = new Milk_Foam(beverage);
+                    break;
+
+                case DrinkType.Freddo:
+                    beverage = new Espresso();
+                    beverage = new Liqour(beverage);
+                    beverage = new Ice_Cream(beverage);
+                    break;
+
+                case DrinkType.Frappuccino:
+                    beverage = new Espresso();
+                    beverage = new Ice(beverage);
+                    beverage = new Steamed_Milk(beverage);
+                    beverage = new Whip(beverage);
+                    break;
+
+                case DrinkType.CaramelFrappuccino:
+                    beverage = new Espresso();
+                    beverage = new Ice(beverage);
+                    beverage = new Steamed_Milk(beverage);
+                    beverage = new Cream(beverage);
+                    beverage = new Syrup(beverage);
+                    break;
+
+                case DrinkType.Frappe:
+                    beverage = new Espresso();
+                    beverage = new Steamed_Milk(beverage);
+                    beverage = new Steamed_Milk(beverage);
+                    beverage = new Ice_Cream(beverage);
+                    break;
+
+                case DrinkType.IrishCoffee:
+                    beverage = new Espresso();
+                    beverage = new Whiskey(beverage);
+                    beverage = new Whip(beverage);
+                    beverage = new EspressoCondiment(beverage);
+                    break;
+
+                default:
+                    throw new ArgumentException("Invalid drink type");
+            }
+            beverage.size = size;
+            PrintBeverage(beverage);
+
+            return beverage;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,44 @@
+﻿using DecoratorPattern.Beverages;
+using DecoratorPattern.Condiments;
+
+namespace DecoratorPattern
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            BeverageFactory factory = new();
+
+            Beverage espresso = factory.createBeverage(DrinkType.Espresso, Size.TALL);
+            Beverage doppio = factory.createBeverage(DrinkType.Doppio, Size.GRANDE);
+            Beverage lungo = factory.createBeverage(DrinkType.Lungo, Size.VENDI);
+            Beverage macchiato = factory.createBeverage(DrinkType.Macchiato, Size.TALL);
+            Beverage corretta = factory.createBeverage(DrinkType.Corretta, Size.VENDI);
+            Beverage conPanna = factory.createBeverage(DrinkType.ConPanna, Size.GRANDE);
+            Beverage cappuccino = factory.createBeverage(DrinkType.Cappuccino, Size.VENDI);
+            Beverage americano = factory.createBeverage(DrinkType.Americano, Size.TALL);
+            Beverage caffeLatte = factory.createBeverage(DrinkType.CaffeLatte, Size.GRANDE);
+            Beverage flatWhite = factory.createBeverage(DrinkType.FlatWhite, Size.TALL);
+            Beverage romana = factory.createBeverage(DrinkType.Romana, Size.VENDI);
+            Beverage marocchino = factory.createBeverage(DrinkType.Marocchino, Size.GRANDE);
+            Beverage mocha = factory.createBeverage(DrinkType.Mocha, Size.TALL);
+            Beverage bicerin = factory.createBeverage(DrinkType.Bicerin, Size.VENDI);
+            Beverage breve = factory.createBeverage(DrinkType.Breve, Size.GRANDE);
+            Beverage rafCoffee = factory.createBeverage(DrinkType.RafCoffee, Size.VENDI);
+            Beverage meadRaf = factory.createBeverage(DrinkType.MeadRaf, Size.TALL);
+            Beverage galao = factory.createBeverage(DrinkType.Galao, Size.GRANDE);
+            Beverage caffeAffogato = factory.createBeverage(DrinkType.CaffeAffogato, Size.VENDI);
+            Beverage viennaCoffee = factory.createBeverage(DrinkType.ViennaCoffee, Size.TALL);
+            Beverage glace = factory.createBeverage(DrinkType.Glace, Size.GRANDE);
+            Beverage chocolateMilk = factory.createBeverage(DrinkType.ChocolateMilk, Size.VENDI);
+            Beverage demiCreme = factory.createBeverage(DrinkType.DemiCreme, Size.GRANDE);
+            Beverage latteMacchiato = factory.createBeverage(DrinkType.LatteMacchiato, Size.TALL);
+            Beverage freddo = factory.createBeverage(DrinkType.Freddo, Size.VENDI);
+            Beverage frappuccino = factory.createBeverage(DrinkType.Frappuccino, Size.GRANDE);
+            Beverage caramelFrappuccino = factory.createBeverage(DrinkType.CaramelFrappuccino, Size.TALL);
+            Beverage frappe = factory.createBeverage(DrinkType.Frappe, Size.VENDI);
+            Beverage irishCoffee = factory.createBeverage(DrinkType.IrishCoffee, Size.GRANDE);
+
+        }
+    }
+}
