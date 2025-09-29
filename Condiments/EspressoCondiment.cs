588e210b2b9f0210ce73896989cb4cca502a8584
Daan Reuvers <77575704+DaanRVRS@users.noreply.github.com>
2025-09-29 14:18:21 +0200

    Add files via upload

---
+++ Condiments/EspressoCondiment.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using DecoratorPattern.Beverages;
+using System.Threading.Tasks;
+
+namespace DecoratorPattern.Condiments
+{
+    internal class EspressoCondiment : CondimentDecorator
+    {
+        public EspressoCondiment(Beverage beverage)
+        {
+            this.baseBeverage = beverage;
+        }
+        public override double cost()
+        {
+            double extra = 0.10;
+            switch (baseBeverage.Size)
+            {
+                case Size.GRANDE:
+                    extra = 0.20;
+                    break;
+                case Size.VENDI:
+                    extra = 0.30;
+                    break;
+            }
+            return extra + baseBeverage.cost();
+        }
+        public override string GetDescription()
+        {
+            return baseBeverage.GetDescription() + ", Espresso Shot";
+        }
+    }
+}
---
+++ Condiments/Milk Foam.cs
@@ -0,0 +1,35 @@
+﻿using DecoratorPattern.Beverages;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DecoratorPattern.Condiments
+{
+    internal class Milk_Foam : CondimentDecorator
+    {
+        public Milk_Foam(Beverage Beverage)
+        {
+            this.baseBeverage = Beverage;
+        }
+        public override double cost()
+        {
+            double extra = 0.10;
+            switch (baseBeverage.Size)
+            {
+                case Size.GRANDE:
+                    extra = 0.20;
+                    break;
+                case Size.VENDI:
+                    extra = 0.30;
+                    break;
+            }
+            return extra + baseBeverage.cost();
+        }
+        public override string GetDescription()
+        {
+            return baseBeverage.GetDescription() + ", Milk Foam";
+        }
+    }
+}
