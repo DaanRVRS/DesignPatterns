37e8467dc4098ee3a1c34119235ee69f56c2e6db
Daan Reuvers <77575704+DaanRVRS@users.noreply.github.com>
2025-09-29 14:17:25 +0200

    Add files via upload

---
+++ Beverages/Beverage.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DecoratorPattern.Beverages
+{
+    public enum Size
+    {
+        TALL,
+        GRANDE,
+        VENDI
+    }
+    internal abstract class Beverage
+    {
+        public Size Size { get { return size; } set { size = value; } }
+        public Size size;
+
+        protected string description = "Unknown";
+        protected Beverage baseBeverage = null;
+        
+
+        public virtual string GetDescription()
+        {
+            return description;
+        }
+        public virtual string GetSize()
+        {
+            return Size.ToString();
+        }
+
+        public abstract double cost();
+    }
+}
